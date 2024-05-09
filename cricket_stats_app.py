fd0fcea0a06bbbe0e43092d9f8bb1da5c0b17bc3
Ankit Gujare <80085008+anky19698@users.noreply.github.com>
2024-05-09 09:41:40 +0530

    Update cricket_stats_app.py

--- cricket_stats_app.py
+++ cricket_stats_app.py
@@ -12,6 +12,7 @@ ca = certifi.where()
 mongo_uri = st.secrets['mongo_uri']
 key = st.secrets['key']
 
+
 def load_database_collection(mongo_uri):
     # Connect to MongoDB
     ca = certifi.where()
@@ -27,15 +28,16 @@ def load_database_collection(mongo_uri):
 
     return collection
 
+
 def store_message(collection, message):
     # Insert message into MongoDB collection
     collection.insert_one(message)
 
 
-
 def connect_to_database(database_name):
     conn = sqlite3.connect(database_name)
     return conn
+
 
 def query_database(conn, query):
     cursor = conn.cursor()
@@ -45,9 +47,7 @@ def query_database(conn, query):
     return column_names, data
 
 
-
 def filter_database(user_input):
-
     genai.configure(api_key=key)
     model = genai.GenerativeModel('gemini-pro')
 
@@ -56,83 +56,81 @@ def filter_database(user_input):
     table_info = """
 
     TABLE: batter_vs_venue
-    
-    striker,stadium,innings,runs_scored,balls_faced,dismissals,dots,fours,sixes,fifties,hundreds,batting_AVG,batting_SR,dot_percentage
+
+    striker,stadium,innings,runs_scored,balls_faced,wickets_taken,dots,fours,sixes,fifties,hundreds,batting_AVG,batting_SR,dot_percentage
     A Ashish Reddy,Arun Jaitley Stadium,1,16,9,1,1,2,0,0,0,16.0,177.77777777777777,11.11111111111111
-    
+
     TABLE: batter_vs_team
-    
-    striker,bowling_team,innings,runs_scored,balls_faced,dismissals,dots,fours,sixes,fifties,hundreds,batting_AVG,batting_SR,dot_percentage
+
+    striker,bowling_team,innings,runs_scored,balls_faced,wickets_taken,dots,fours,sixes,fifties,hundreds,batting_AVG,batting_SR,dot_percentage
     A Ashish Reddy,Chennai Super Kings,3,45,25,2,8,3,3,0,0,22.5,180.0,32.0
-    
+
     TABLE: batter_vs_bowler
-    
-    striker,bowler,innings,runs_scored,balls_faced,dismissals,dots,fours,sixes,batting_SR,dot_percentage,inning_vs_dismissal
+
+    striker,bowler,innings,runs_scored,balls_faced,wickets_taken,dots,fours,sixes,batting_SR,dot_percentage,inning_vs_dismissal
     A Ashish Reddy,A Nehra,2,7,9,1,5,1,0,77.77777777777777,55.55555555555556,1
-    
+
     TABLE: bowling_record
-    
-    bowler,innings,runs_conceded,balls_bowled,dismissals,dots,fours,sixes,Economy,bowling_AVG
+
+    bowler,innings,runs_conceded,balls_bowled,wickets_taken,dots,fours,sixes,Economy,bowling_AVG
     A Ashish Reddy,20,400,270,18,89,26,20,8.88888888888889,22.22222222222222
-    
+
     TABLE: batting_record
-    
-    striker,innings,runs_scored,balls_faced,dismissals,dots,fours,fifties,hundreds,sixes,batting_SR,dot_percentage,batting_AVG
+
+    striker,innings,runs_scored,balls_faced,wickets_taken,dots,fours,fifties,hundreds,sixes,batting_SR,dot_percentage,batting_AVG
     A Ashish Reddy,23,280,196,13,61,16,2,1,15,142.85714285714286,31.122448979591837,21.53846153846154
-    
+
     TABLE: bowling_record_by_year
 
-    bowler, year, innings,runs_conceded,balls_bowled,dismissals,dots,fours,sixes,Economy,bowling_AVG
+    bowler, year, innings,runs_conceded,balls_bowled,wickets_taken,dots,fours,sixes,Economy,bowling_AVG
     A Ashish Reddy, 2022, 20,400,270,18,89,26,20,8.88888888888889,22.22222222222222
-    
-    TABLE: batting_record_by_year
-    
-    striker, year, innings,runs_scored,balls_faced,dismissals,dots,fours, fifties, hundreds, sixes,batting_SR,dot_percentage,batting_AVG
-    A Ashish Reddy, 2022, 23,280,196,13,61,16,2,1,15,142.85714285714286,31.122448979591837,21.53846153846154
-    
-    """
 
+    TABLE: batting_record_by_year
+
+    striker, year, innings,runs_scored,balls_faced,wickets_taken,dots,fours, fifties, hundreds, sixes,batting_SR,dot_percentage,batting_AVG
+    A Ashish Reddy, 2022, 23,280,196,13,61,16,2,1,15,142.85714285714286,31.122448979591837,21.53846153846154
+
+    """
 
     # user_input = "kl rahul vs jasprit bumrah Record"
 
     input_prompt = f"""
     You are a Smart AI Cricket Analyst, Working on IPL(Indian Premier League) Data
-    
+
     You're tasked with generating a SQL query based on the user input to filter cricket data from the database. Your goal is to extract Player Names, Stadium Names, and Team Names from the user input and filter accordingly.
-    
+
     Remember, each row in the database represents a Ball Event, and you should only use the specified columns to construct the SQL query.
-    
+
     Here's the Database Table Information: {table_info}
-    
+
     For Player Names, use the following format (LastName is always complete):
     [initial Letter of firstName + any letters in between + LastName]
     Example: For a player named Sunil Narine, use "S% Narine"
-    
+
     Example user input:
     1) virat Kohli vs jasprit bumrah Record
-    
+
     Expected SQL Output:
     Select * From batter_vs_bowler where striker LIKE "V% Kohli" AND bowler LIKE "J% Bumrah"
 
     2) virat kohli runs in ipl 2024
-    
+
     Expected SQL Output:
     Select * From batting_record_by_year where striker LIKE "V% Kohli" AND year=2024
 
     3) jasprit bumrah wickets in ipl
-    
+
     Expected SQL Output:
     Select * From bowling_record where bowler LIKE "J% Bumrah"
-    
+
     4) most sixes hit by player: In this case, you should apply sort in SQL query, and retrieve only top 10 rows 
-    
+
     Remember, only use filters or sorts in the SQL query, and do not use any type of JOIN operations.
-    
+
     Ensure that the SQL query is written only for the relevant table and includes columns and table names only from the Table Information.
-    
+
     [user input] = {user_input}
     """
-
 
     response = model.generate_content(input_prompt)
 
@@ -160,6 +158,7 @@ def analyze_result(df, user_input):
 
     return response
 
+
 def main():
     st.title("Cricket Stats Assistant")
 
@@ -169,7 +168,7 @@ def main():
     instructions = """
     This is Cricket(IPL) Stats Assistant, which answers your cricket stats queries.
     It has Latest Data up to 2 previous days.
-    
+
     You Can Ask Queries like: 
     1) Virat Kohli runs in ipl 2024
     2) virat kohli vs jasprit bumrah
@@ -182,11 +181,11 @@ def main():
     2) Avoid using short names or nick names
     3) Take above queries as a reference
     """
-    
+
     if instructions_button:
         # Display the text if the button is clicked
         st.text_area("Instructions", value=instructions, height=300, disabled=True)
-    
+
     # Load Database of Chats
     collection = load_database_collection(mongo_uri)
 
@@ -195,7 +194,6 @@ def main():
 
     # # User input
     # user_input = st.text_input("Ask something about cricket:")
-
 
     # # If user submits a query
     # if st.button("Ask"):
@@ -232,36 +230,35 @@ def main():
         try:
             with st.chat_message("user"):
                 st.markdown(user_input)
-    
+
             # Display assistant response in chat message container
             with st.chat_message("assistant"):
-    
-                    sql_query = filter_database(user_input)
-                    # Query the database
-                    if sql_query:
-                        column_names, result = query_database(conn, sql_query)
-    
-                        # Create DataFrame
-                        df = pd.DataFrame(result, columns=column_names)
-    
-                        # Display result in a table
-                        # st.write(df)
-    
-                        content = analyze_result(df, user_input)
-                        st.write(content.text)
-                        # Store user message and assistant response
-                        store_message(collection,
-                                      {"User": user_input,
-                                       "AI": content.text})
-                        st.write(df)
-    
-                    else:
-                        st.write("Sorry, I couldn't understand your query.")
-    
-    
+
+                sql_query = filter_database(user_input)
+                # Query the database
+                if sql_query:
+                    column_names, result = query_database(conn, sql_query)
+
+                    # Create DataFrame
+                    df = pd.DataFrame(result, columns=column_names)
+
+                    # Display result in a table
+                    # st.write(df)
+
+                    content = analyze_result(df, user_input)
+                    st.write(content.text)
+                    # Store user message and assistant response
+                    store_message(collection,
+                                  {"User": user_input,
+                                   "AI": content.text})
+                    st.write(df)
+
+                else:
+                    st.write("Sorry, I couldn't understand your query.")
+
             # Add assistant response to chat history
             st.session_state.messages.append({"role": "assistant", "content": content.text})
-    
+
         except:
             st.write("This Question Was a Googly, Please Try Some Another Delivery")
 
