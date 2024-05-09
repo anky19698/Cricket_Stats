99533c32e1c7b48445e71aab7d18bb98f17bba03
Ankit Gujare <80085008+anky19698@users.noreply.github.com>
2024-05-09 18:19:07 +0530

    Update cricket_stats_app.py

--- cricket_stats_app.py
+++ cricket_stats_app.py
@@ -151,8 +151,10 @@ def analyze_result(df, user_input):
     model = genai.GenerativeModel('gemini-pro')
     prompt = f"""
     Act as a Smart AI Cricket Stats Assistant, 
-    Based on Previous {user_input}, You have Successfully got a resulting Dataframe: {df}
-    Analyze this Dataframe about cricket records, Give a brief summary highlighting only important stats and be 100% accurate, in 3-4 lines
+    Based on Previous User Input: {user_input}, You have Successfully got a resulting Dataframe: {df}
+    Analyze this Dataframe df about cricket(IPL) records, Give a brief summary highlighting only important stats like runs_scored, wickets_taken and then other key stats
+    and be 100% accurate, in 3-4 lines
+    Consider the Table Info: {table_info} to understand the dataframe.
     Note: Only If the {df} is empty, then just ask them to retry in quirky cricket way, like this was a bouncer please try again!
     """
     response = model.generate_content(prompt)
